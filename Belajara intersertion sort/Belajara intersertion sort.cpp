cacbdbcd2f662ae429fded5824863c50283d884e
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-03-24 15:48:26 +0700

    memasukan jumlah i =0, i< n; i++ untuk for

--- Belajara intersertion sort/Belajara intersertion sort.cpp
+++ Belajara intersertion sort/Belajara intersertion sort.cpp
@@ -25,3 +25,10 @@ void input() {
 	cout << "=======================" << endl;
 	cout << "Masukan Element Array  " << endl;
 	cout << "=======================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
